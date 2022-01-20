ad9a2b2a85b2243fedcc9e0c35ca9df1a3f5e5e9
katethegreat1 <katehanthegreat1@gmail.com>
2022-01-21 10:57:56 +1300

    init repo

---
+++ db/index.js
@@ -0,0 +1,31 @@
+const config = require('./knexfile').development
+const database = require('knex')(config)
+
+
+function getAllLocations (db = database) {
+    return db('locations')
+        .select()
+}
+
+
+function getEventsByDay(day, db = database) {
+    return db('events')
+        .join('locations', 'events.location_id', 'locations.id')
+        .where('day', day)
+        .select(
+            'events.id as eventId',
+            'day',
+            'time',
+            'events.name as eventName',
+            'events.description as eventDescription',
+            'locations.id as locationsId',
+            'locations.name as locationsName',
+            'locations.description as locationsDescription'
+        )
+}
+
+
+module.exports = {
+    getAllLocations,
+    getEventsByDay,
+}
