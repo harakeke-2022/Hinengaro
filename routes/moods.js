ca8fc46d82729c676600bd6e517a3173a95203f1
cl-lin <clin3105@gmail.com>
2022-01-21 13:38:33 +1300

    add comments and colour database

--- routes/moods.js
+++ routes/moods.js
@@ -1,46 +1,39 @@
 const express = require('express')
-const db = require('../db/index')   // index.js
+const db = require('../db/index') // index.js
 const router = express.Router()
 
-//const { eventDays, capitalise, validateDay } = require('../helpers')
+// const { eventDays, capitalise, validateDay } = require('../helpers')
 
 module.exports = router
 
-
-
 // Our main page routes go here
 router.get('/', async (req, res) => {
-    try {
-      const comments = await db.getAllComments()
-      const viewData = { comments }
-      res.render('showComments', viewData)
-    } catch (error) {
-      res.redirect('/error')
-      console.error(error)
+  try {
+    const comments = await db.getAllComments()
+    const viewData = { comments }
+    res.render('showComments', viewData)
+  } catch (error) {
+    res.redirect('/error')
+    console.error(error)
+  }
+})
+
+// Generate random comments - second page
+router.get('/', async (req, res) => {
+  try {
+    const comments = await db.getAllComments()
+    const viewData = { comments }
+    function getRandomInt (max) {
+      return Math.floor(Math.random() * max)
     }
-  })
+    res.redirect(`/${getRandomInt(10) + 1}`)
+  } catch (error) {
+    res.redirect('/error')
+    console.error(error)
+  }
+})
 
-
-  // Generate random comments - second page
-  router.get('/', async (req, res) => {
-    try {
-        const comments = await db.getAllComments()
-        const viewData = { comments }
-        function getRandomInt(max) {
-            return Math.floor(Math.random() * max);
-        }
-            res.redirect(`/${getRandomInt(10) + 1}`) 
-        } catch (error) {
-            res.redirect('/error')
-            console.error(error)
-        }
-    })
-
-
-
-
-//Our routes go here
-
+// Our routes go here
 
 //   router.get('/:id', async (req, res) => {
 //     const id = Number(req.params.id) - 1
@@ -54,13 +47,13 @@ router.get('/', async (req, res) => {
 //     }
 //   })
 
-  // Route for individual restaurants
+// Route for individual restaurants
 // router.get('/:id', (req, res) => {
 //   const id = Number(req.params.id) - 1
 //   fsPromises.readFile('./data.json', 'utf8')
 //     .then((foodSpots) => {
 //       const realFoodData = JSON.parse(foodSpots)
-//       res.render('details', realFoodData.restaurants[id])    
+//       res.render('details', realFoodData.restaurants[id])
 //       return null
 //     })
 //     .catch ((err) => {
@@ -69,16 +62,9 @@ router.get('/', async (req, res) => {
 // })
 
 // Generate random ID for button
-// router.get('/random', (req,res)=> {        
+// router.get('/random', (req,res)=> {
 //   function getRandomInt(max) {
 //     return Math.floor(Math.random() * max);
 //      }
 //      res.redirect(`/${getRandomInt(10) + 1}`)
-// }) 
-
-
-
-
-
-
-
+// })
